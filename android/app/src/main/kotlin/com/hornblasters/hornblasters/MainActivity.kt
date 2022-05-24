508cef246cd746b0c1cb06ec03c5b647f32c2c4b
NismoLap <nismolap@gmail.com>
2022-05-24 10:21:40 +0200

    Setup Project
    Setup Project

---
+++ android/app/src/main/kotlin/com/hornblasters/hornblasters/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hornblasters.hornblasters
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/UI/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//Primary Colors
+const P1 = Color(0xFF593685);
+const P2 = Color(0xFFF1EEF3);
+const P3 = Color(0xFFFFFFFF);
+const P4 = Color(0xFFF48B2B);
+const P5 = Color(0xFFF48B2B);
+
+//Secondary Colors
+const S1 = Color(0xFF593685);
+const S2 = Color(0xFFF1EEF3);
+const S3 = Color(0xFFFFFFFF);
+const S4 = Color(0xFFF48B2B);
+
+const defaultPadding = 16.0;
